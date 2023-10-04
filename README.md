960ae61041589d705c97132c0a75b4b4f50f25e5
shreyasbarve <shreyasbarve13@gmail.com>
2023-10-05 00:06:29 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# asteroid-game
+
+Made using plain HTML and JavaScript following Youtube NetNinja
---
+++ script.js
@@ -0,0 +1,394 @@
+const canvas = document.querySelector("canvas");
+// console.log("Canvas", canvas);
+
+const c = canvas.getContext("2d");
+// console.log("Canvas Context", c);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+    this.rotation = 0;
+  }
+
+  draw() {
+    c.save();
+    c.translate(this.position.x, this.position.y);
+    c.rotate(this.rotation);
+    c.translate(-this.position.x, -this.position.y);
+
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, 100, 100);
+    c.beginPath();
+    c.moveTo(this.position.x + 30, this.position.y);
+    c.lineTo(this.position.x - 10, this.position.y - 10);
+    c.lineTo(this.position.x - 10, this.position.y + 10);
+    c.closePath();
+    c.strokeStyle = "white";
+    c.stroke();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+
+  getVertices() {
+    const cos = Math.cos(this.rotation);
+    const sin = Math.sin(this.rotation);
+
+    return [
+      {
+        x: this.position.x + cos * 30 - sin * 0,
+        y: this.position.y + sin * 30 + cos * 0,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * 10,
+        y: this.position.y + sin * -10 + cos * 10,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * -10,
+        y: this.position.y + sin * -10 + cos * -10,
+      },
+    ];
+  }
+}
+
+class Projectile {
+  constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 5;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.fillStyle = "white";
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class Asteroid {
+  constructor({position, velocity, radius}) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = radius;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.strokeStyle = "red";
+    c.stroke();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+const player = new Player({
+  position: {x: canvas.width / 2, y: canvas.height / 2},
+  velocity: {x: 0, y: 0},
+});
+
+// console.log(player);
+
+player.draw();
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const SPEED = 3;
+const ROTATION = 0.05;
+const FRICTION = 0.97;
+const PROJECTILE_SPEED = 3;
+
+const projectiles = [];
+const asteroids = [];
+
+const intervalId = setInterval(() => {
+  const index = Math.floor(Math.random() * 4);
+  let x, y;
+  let vx, vy;
+  let radius = 50 * Math.random() + 10;
+
+  switch (index) {
+    case 0: // left of the screen
+      x = 0 - radius;
+      y = Math.random() * canvas.height;
+      vx = 1;
+      vy = 0;
+      break;
+    case 1: // bottom of the screen
+      x = Math.random() * canvas.width;
+      y = canvas.height + radius;
+      vx = 0;
+      vy = -1;
+      break;
+    case 2: // right of the screen
+      x = canvas.width + radius;
+      y = Math.random() * canvas.height;
+      vx = 1;
+      vy = 0;
+      break;
+    case 3: // top of the screen
+      x = Math.random() * canvas.width;
+      y = canvas.height + radius;
+      vx = 0;
+      vy = -1;
+      break;
+  }
+
+  asteroids.push(
+    new Asteroid({
+      position: {
+        x: x,
+        y: y,
+      },
+      velocity: {
+        x: vx,
+        y: vy,
+      },
+      radius,
+    })
+  );
+
+  // console.log(asteroids);
+}, 3000);
+
+function circleCollision(circle1, circle2) {
+  const xDifference = circle2.position.x - circle1.position.x;
+  const yDifference = circle2.position.y - circle1.position.y;
+
+  const distance = Math.sqrt(
+    Math.pow(xDifference, 2) + Math.pow(yDifference, 2)
+  );
+
+  if (distance <= circle1.radius + circle2.radius) {
+    // console.log("two have collided");
+    return true;
+  }
+
+  return false;
+}
+
+function circleTriangleCollision(circle, triangle) {
+  // Check if the circle is colliding with any of the triangle's edges
+  for (let i = 0; i < 3; i++) {
+    let start = triangle[i];
+    let end = triangle[(i + 1) % 3];
+
+    let dx = end.x - start.x;
+    let dy = end.y - start.y;
+    let length = Math.sqrt(dx * dx + dy * dy);
+
+    let dot =
+      ((circle.position.x - start.x) * dx +
+        (circle.position.y - start.y) * dy) /
+      Math.pow(length, 2);
+
+    let closestX = start.x + dot * dx;
+    let closestY = start.y + dot * dy;
+
+    if (!isPointOnLineSegment(closestX, closestY, start, end)) {
+      closestX = closestX < start.x ? start.x : end.x;
+      closestY = closestY < start.y ? start.y : end.y;
+    }
+
+    dx = closestX - circle.position.x;
+    dy = closestY - circle.position.y;
+
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance <= circle.radius) {
+      return true;
+    }
+  }
+
+  // No collision
+  return false;
+}
+
+function isPointOnLineSegment(x, y, start, end) {
+  return (
+    x >= Math.min(start.x, end.x) &&
+    x <= Math.max(start.x, end.x) &&
+    y >= Math.min(start.y, end.y) &&
+    y <= Math.max(start.y, end.y)
+  );
+}
+
+// function to run at every frame after window loads
+function animate() {
+  const animationId = window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+
+  // projectile management
+  for (let i = projectiles.length - 1; i >= 0; i--) {
+    const projectile = projectiles[i];
+    projectile.update();
+
+    // garbage collection for projectiles
+    if (
+      projectile.position.x + projectile.radius < 0 ||
+      projectile.position.x - projectile.radius > canvas.width ||
+      projectile.position.y - projectile.radius > canvas.height ||
+      projectile.position.y + projectile.radius < 0
+    ) {
+      projectiles.splice(i, 1);
+    }
+  }
+
+  // Asteroid Mangement
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+    const asteroid = asteroids[i];
+    asteroid.update();
+
+    if (circleTriangleCollision(asteroid, player.getVertices())) {
+      // console.log("Game Over");
+      window.cancelAnimationFrame(animationId);
+      window.clearInterval(intervalId);
+      alert("Game Over");
+    }
+
+    // for projectiles
+    for (let j = projectiles.length - 1; j >= 0; j--) {
+      const projectile = projectiles[j];
+
+      if (circleCollision(asteroid, projectile)) {
+        // console.log("BIG SUCCESS");
+        asteroids.splice(i, 1);
+        projectiles.splice(j, 1);
+      }
+    }
+
+    // garbage collection for asteroids
+    if (
+      asteroid.position.x + asteroid.radius < 0 ||
+      asteroid.position.x - asteroid.radius > canvas.width ||
+      asteroid.position.y - asteroid.radius > canvas.height ||
+      asteroid.position.y + asteroid.radius < 0
+    ) {
+      asteroids.splice(i, 1);
+    }
+  }
+
+  // Math functions are used to position the ship relative to its rotation
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED;
+    player.velocity.y = Math.sin(player.rotation) * SPEED;
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION;
+    player.velocity.y *= FRICTION;
+  }
+  if (keys.s.pressed) {
+    player.velocity.x = -Math.cos(player.rotation) * SPEED;
+    player.velocity.y = -Math.sin(player.rotation) * SPEED;
+  } else if (!keys.s.pressed) {
+    player.velocity.x *= FRICTION;
+    player.velocity.y *= FRICTION;
+  }
+
+  if (keys.d.pressed) player.rotation += ROTATION;
+  else if (keys.a.pressed) player.rotation -= ROTATION;
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      // console.log("w was pressed");
+      keys.w.pressed = true;
+      break;
+    case "KeyS":
+      // console.log("w was pressed");
+      keys.s.pressed = true;
+      break;
+    case "KeyA":
+      // console.log("a was pressed");
+      keys.a.pressed = true;
+      break;
+    case "KeyD":
+      // console.log("d was pressed");
+      keys.d.pressed = true;
+      break;
+    case "Space":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + Math.cos(player.rotation) * 30,
+            y: player.position.y + Math.sin(player.rotation) * 30,
+          },
+          velocity: {
+            x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+            y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+          },
+        })
+      );
+      // console.log(projectiles);
+      break;
+  }
+
+  // console.log(event);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      // console.log("w was pressed");
+      keys.w.pressed = false;
+      break;
+    case "KeyS":
+      // console.log("w was pressed");
+      keys.s.pressed = false;
+      break;
+    case "KeyA":
+      // console.log("a was pressed");
+      keys.a.pressed = false;
+      break;
+    case "KeyD":
+      // console.log("d was pressed");
+      keys.d.pressed = false;
+      break;
+  }
+
+  // console.log(event);
+});
